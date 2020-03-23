8780f7f7a48e663b8f9605b1cbdfc9089af8a730
Pazbear <mkcho1997@daum.net>
2020-03-23 21:43:06 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://pazbear:mk2713!@@practice-czslx.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false
+}).then(()=>console.log('MongoDB Connected...'))
+.catch(err=>console.log(err));
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, ()=>console.log("server is running..."));
